ca9a5851b733fefc05a602bc9faa1b8fc8191f1e
juliamistieri <julia.mistieri02@gmail.com>
2024-02-28 17:41:59 -0400

    aula dia 28/02

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    images: {
+        domains: [
+            "www.ifms.edu.br", "www.ifms.edu.br"
+        ]
+    }
+
+}
 
 module.exports = nextConfig
--- src/app/componentes/Footer.js
+++ src/app/componentes/Footer.js
@@ -1,11 +1,17 @@
+import Link from "next/link";
+import styles from "./footer.module.css"
+import Image from "next/image";
+
 export default function Footer(){
     return(
-        <header>
+        <header className={styles.footercab}>
+          <Image width={100} height={100} src={"https://www.ifms.edu.br/marcaifms.png"}/>
         <h1>Footer</h1>
           <nav>
             <ul>
-                <li>Footer</li>
+            <Link href="/">
                 <li>Login</li>
+                </Link>
             </ul>
           </nav>
         </header>
