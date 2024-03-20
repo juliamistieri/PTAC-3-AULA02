7a9bd77ecaaeb73d1249de89a384e66498b434d2
juliamistieri <julia.mistieri02@gmail.com>
2024-03-20 17:37:25 -0400

    update lalalalala

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Image from "next/Image"
 export default async function Home() {
   const respostas = await fetch("https://back-end-ifms-rho.vercel.app/campi",{
     next:{
       revalidate: 1
     }
-  });
+  })
   const campi = await respostas.json();
   return (
     <main>
@@ -11,7 +12,7 @@ export default async function Home() {
       {campi.map((campus) => (
         <div>
           <p>{campus.nome_campus}</p>
-          <Image width={100} height={100} src="campus.image_url"></Image>
+          <Image width={100} height={100} src={campus.image_url}/>
         </div>
       ))}
     </main>
